354d3ccabff809c8337d9ddb845cb65995909752
rmcelheny <rosaNEW@Rosas-MBP.wireless.yale.internal>
2017-03-27 21:44:02 -0400

    test

--- project3/js/script.js
+++ project3/js/script.js
@@ -6,55 +6,66 @@ $(function(){
     $('.spin.middle').attr('class', 'middle spin');
   }
 
-  $('#slowest').mouseover(function(){
-  	document.getElementById('secreto').playbackRate = 0.5;
-    cleanState();
-    $('.spin').addClass('slowest');
-  });
+    $('#slowest').mouseover(function(){
+      	document.getElementById('secreto').playbackRate = 0.5;
+        cleanState();
+        $('.spin').addClass('slowest');
+    });
 
-  $('#slower').mouseover(function(){
-    document.getElementById('secreto').playbackRate = 0.75;
-    cleanState();
-    $('.spin').addClass('slower');
-  });
+    $('#slower').mouseover(function(){
+      document.getElementById('secreto').playbackRate = 0.75;
+      cleanState();
+      $('.spin').addClass('slower');
+    });
 
-  $('#slow').mouseover(function(){
-  	document.getElementById('secreto').playbackRate = 0.9;
-    cleanState();
-    $('.spin').addClass('slow');
-  });
+    $('#slow').mouseover(function(){
+    	document.getElementById('secreto').playbackRate = 0.9;
+      cleanState();
+      $('.spin').addClass('slow');
+    });
 
-  $('#normal').mouseover(function(){
-  	document.getElementById('secreto').playbackRate = 1;
-    cleanState();
-    $('.spin').addClass('normal');
-  });
+    $('#normal').mouseover(function(){
+    	document.getElementById('secreto').playbackRate = 1;
+      cleanState();
+      $('.spin').addClass('normal');
+    });
 
-  $('#fast').mouseover(function(){
-  	document.getElementById('secreto').playbackRate = 1.25;
-    cleanState();
-    $('.spin').addClass('fast');
-  });
+    $('#fast').mouseover(function(){
+    	document.getElementById('secreto').playbackRate = 1.25;
+      cleanState();
+      $('.spin').addClass('fast');
+    });
 
-  $('#faster').mouseover(function(){
-  	document.getElementById('secreto').playbackRate = 2;
-    cleanState();
-    $('.spin').addClass('faster');
-  });
+    $('#faster').mouseover(function(){
+    	document.getElementById('secreto').playbackRate = 2;
+      cleanState();
+      $('.spin').addClass('faster');
+    });
 
 });
 
-    function aud_play_pause() {
-      var myAudio = document.getElementById("secreto");
-      if (myAudio.paused) {
+$('.containercontrol').click(function(){
+        $('.spin').toggleClass('pause');
+        var myAudio = document.getElementById("secreto");
+        if (myAudio.paused) {
           myAudio.play();
         } else {
           myAudio.pause();
         }
-    }
+    });
 
- $(function(){
-    $('.containercontrol').click(function(){
-        $('.spin').toggleClass('pause')
-    })
- })
+
+
+//  $(document).ready(function(){
+//   $(window).scroll(function() {
+//     if ($(document).scrollTop() => 1200) {
+//       $("body").addClass("test");
+//     } else {
+//       $("body").removeClass("test");
+//     }
+//   });
+// });
+
+
+
+

42da044bd24c23cf64b455b9a8d74bb7f1eb1900
rmcelheny <rosaNEW@Rosas-MacBook-Pro.local>
2017-04-16 00:53:00 -0400

    reading response

---
+++ project4/js/apr19.js
@@ -0,0 +1,147 @@
+ $( document ).ready(function() {
+
+    // $("#wart").click(function(){
+    // 	$("#art").show();
+    // });
+
+    // $("#wartists").click(function(){
+    // 	$("#artists").show();
+    // });
+
+    $("#wgalleries").click(function(){
+    	$(this).hide();
+    	$("#galleries").css('display','inline-block');
+    });
+
+    $("#wmuseums").click(function(){
+     	$(this).hide();
+    	$("#museums").css('display','inline-block');
+    });
+
+    $("#wsolo").click(function(){
+    	$(this).hide();
+    	$("#solo").css('display','inline-block');
+    });
+
+    $("#wgroup").click(function(){
+    	$(this).hide();
+    	$("#group").css('display','inline-block');
+    });
+
+    $("#wwhitney").click(function(){
+    	$(this).hide();
+    	$("#whitney").css('display','inline-block');
+    });
+
+    $("#wvideo").click(function(){
+    	$(this).hide();
+    	$("#video").css('display','inline-block');
+    });
+
+    $("#wfoam").click(function(){
+    	$(this).hide();
+    	$("#foam").css('display','inline-block');
+    });
+
+    $("#winstallation").click(function(){
+    	$(this).hide();
+    	$("#installation").css('display','inline-block');
+    });
+
+    $("#wfunny").click(function(){
+    	$(this).hide();
+    	$("#funny").css('display','inline-block');
+    });
+
+    $("#wpaintings").click(function(){
+    	$(this).hide();
+    	$("#paintings").css('display','inline-block');
+    });
+
+    $("#wcollage").click(function(){
+    	$(this).hide();
+    	$("#collage").css('display','inline-block');
+    });
+
+    $("#wdocumentary").click(function(){
+    	$(this).hide();
+    	$("#documentary").css('display','inline-block');
+    });
+
+    $("#winstitutional").click(function(){
+    	$(this).hide();
+    	$("#institutional").css('display','inline-block');
+    });
+
+    // $("#wartistsinstitute").click(function(){
+    // 	$(this).hide();
+    // 	$("#artistsinstitute").css('display','inline-block');
+    // });
+
+    $("#wlucy").click(function(){
+    	$(this).hide();
+    	$("#lucy").css('display','inline-block');
+    });
+
+    $("#wpenny").click(function(){
+    	$(this).hide();
+    	$("#penny").css('display','inline-block');
+    });
+
+    $("#wgentlewoman").click(function(){
+    	$(this).hide();
+    	$("#gentlewoman").css('display','inline-block');
+    });
+
+    $("#wolson").click(function(){
+    	$(this).hide();
+    	$("#olson").css('display','inline-block');
+    });
+
+    $("#wrow").click(function(){
+    	$(this).hide();
+    	$("#row").css('display','inline-block');
+    });
+
+    $("#wshoppers").click(function(){
+    	$(this).hide();
+    	$("#shoppers").css('display','inline-block');
+    });
+    $("#wdesigner").click(function(){
+    	$(this).hide();
+    	$("#designer").css('display','inline-block');
+    });
+    $("#wmix").click(function(){
+    	$(this).hide();
+    	$("#mix").css('display','inline-block');
+    });
+    $("#wread").click(function(){
+    	$(this).hide();
+    	$("#read").css('display','inline-block');
+    });
+    $("#wcrystal").click(function(){
+    	$(this).hide();
+    	$("#crystal").css('display','inline-block');
+    });
+    $("#wrock").click(function(){
+    	$(this).hide();
+    	$("#rock").css('display','inline-block');
+    });
+    $("#wfanzine").click(function(){
+    	$(this).hide();
+    	$("#fanzine").css('display','inline-block');
+    });
+    $("#wirma").click(function(){
+    	$(this).hide();
+    	$("#irma").css('display','inline-block');
+    });
+    $("#wmorgan").click(function(){
+    	$(this).hide();
+    	$("#morgan").css('display','inline-block');
+    });
+
+
+
+});
+
+
